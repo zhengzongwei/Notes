984b252d9e9188d31382a0301a18f1776a60a032
zhengzongwei <zhengzongwei@foxmail.com>
2021-07-19 14:25:12 +0800

    更新API 测试接口

--- FineOne HCI/service_test/vm.py
+++ FineOne HCI/service_test/vm.py
@@ -7,19 +7,20 @@ import json
 
 
 ##########################################
-####### KVM Snapshot  Test
+####### Service Snapshot  Test
 ##########################################
 
 def print_result(ret):
     print json.dumps(ret,indent=4)
 
-
-
-
 def snapshot_test(uuid):
-    print_result(snapshot_list(uuid))
+    # print_result(snapshot_list(uuid))
 
+    snapshotname = 'init_zzw_test'
+    snapshotmemory = False
+    print_result(snapshot_create_init(uuid,snapshotname,snapshotmemory))
+    # print_result(snapshot_list(uuid))
 
 
 win7_uuid = "291fd1d8-ed1e-11eb-9ba2-0cc47a0c5910"
-snapshot_test(win7_uuid)
+snapshot_test([win7_uuid])
