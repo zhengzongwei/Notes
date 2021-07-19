16459ad3418c74bd3e844c83136cacff9ec53698
zhengzongwei <zhengzongwei@foxmail.com>
2021-07-19 13:22:34 +0800

    添加 测试代码

---
+++ FineOne HCI/service_test/vm.py
@@ -0,0 +1,25 @@
+import os
+import sys 
+sys.path.append("..") 
+
+from mod.vm.vm import *
+import json
+
+
+##########################################
+####### KVM Snapshot  Test
+##########################################
+
+def print_result(ret):
+    print json.dumps(ret,indent=4)
+
+
+
+
+def snapshot_test(uuid):
+    print_result(snapshot_list(uuid))
+
+
+
+win7_uuid = "291fd1d8-ed1e-11eb-9ba2-0cc47a0c5910"
+snapshot_test(win7_uuid)
